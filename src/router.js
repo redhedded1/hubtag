5c1d50c4498d86407fea3a0b489d265511655072
Larry Ball <larry@larryaball.net>
2015-12-08 09:48:22 -0600

    initial commit

---
+++ src/router.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lball on 12/8/15.
+ */
+import Router from 'ampersand-router';
+
+export default Router.extend({
+    routes: {
+        '': 'public',
+        'repos': 'repos'
+    },
+    public(){
+        console.log('public page');
+    },
+    repos(){
+        console.log('repos');
+    }
+})
