58d1f1512af0ccabb7910c8e446676740221b9c6
Larry Ball <larry@larryaball.net>
2015-12-08 09:24:33 -0600

    Beginning state commit

--- src/app.js
+++ src/app.js
@@ -1,2 +1,21 @@
-document.write('Hello Frontend Masters!')
+import Router from './router';
 
+window.app={
+    init(){
+
+    }
+};
+
+window.app.init();
+
+//import React from 'react';
+//import styles from './styles/main.css';
+//
+//const Hello = React.createClass({
+//    render(){
+//        return <div>Hello, {this.props.name}</div>
+//    }
+//});
+//
+//React.render(<Hello name="Whatever Your Name Is"/>, document.body);
+//
